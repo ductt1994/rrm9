d9f88535411a8dee2fe452acd69e29f4363192d2
Duc Thanh Tran <ductt1994@gmail.com>
2021-07-13 20:43:34 +0200

    Added ros package

---
+++ scripts/jetson.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+#Subscriber Function to process the xbox controller values
+def convert(data):
+    # rospy.loginfo(data.linear.x, data.angular.z)
+    print("Speed: {}\t Angle: {}".format(data.linear.x, data.angular.z))
+    # rospy.loginfo(data)
+    # print(data)
+
+    
+def subs():
+    #Declare the node to process the xbox-ctr values
+    rospy.init_node('jetson')
+    
+    #Define the topic name and message type
+    #CALL THE CONVERT FUNCTION !!!
+    rospy.Subscriber('xbox2jetson', Twist, convert)
+    rospy.spin()
+
+if __name__ == '__main__':
+    subs()
---
+++ scripts/xbox_ctr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+#Publisher Function to send joystick values
+def callback(data):
+    twist = Twist()
+    twist.linear.x = data.axes[1]
+    twist.angular.z = data.axes[3]
+    pub.publish(twist)
+
+def start():
+    #Declare the node to publish joystick values from
+    rospy.init_node('xbox_ctr')
+    
+    #Define the topic name and message type
+    global pub
+    pub = rospy.Publisher('xbox2jetson', Twist)
+
+    #Xbox_ctr node gets its values from the Joy-Node and publishes afterwards
+    rospy.Subscriber("joy", Joy, callback)
+
+    #Publish the msg with a rate of 50 Hz
+    rospy.Rate(50)
+    rospy.spin()
+
+if __name__ == '__main__':
+    start()
