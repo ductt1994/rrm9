5c746622d9869315e9e50f74f96165ab1c3f1e94
ductt1994 <ductt1994@gmail.com>
2021-07-15 23:49:10 +0200

    Added camera video stream

--- scripts/cam_test.py
+++ scripts/cam_test.py
@@ -1,6 +1,15 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture(0)
+
+# Set the camera parameters
+dispW = 320
+dispH = 240
+flip = 0
+camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+
+# cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(camSet)
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
