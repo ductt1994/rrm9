6a9379ff68b0c74c73d7b2e3a9d31f079171b5e0
ductt1994 <ductt1994@gmail.com>
2021-07-17 15:09:47 +0200

    Rmoved queue size

--- scripts/xbox_ctr.py
+++ scripts/xbox_ctr.py
@@ -16,7 +16,8 @@ def start():
     
     #Define the topic name and message type
     global pub
-    pub = rospy.Publisher('xbox2jetson', Twist, queue_size = 1)
+    # pub = rospy.Publisher('xbox2jetson', Twist, queue_size = 1)
+    pub = rospy.Publisher('xbox2jetson', Twist)
 
     #Xbox_ctr node gets its values from the Joy-Node and publishes afterwards
     rospy.Subscriber("joy", Joy, callback)
