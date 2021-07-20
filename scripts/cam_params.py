5f86f744f3af39444d3e9d0d3056b284c6cb30a6
ductt1994 <ductt1994@gmail.com>
2021-07-20 23:02:08 +0200

    Added resolution and fps combinations as dict

--- scripts/cam_params.py
+++ scripts/cam_params.py
@@ -1,11 +1,20 @@
 #This file is used to set the capturing and displaying parameters of the IMX219 camera
 
-#Capturing parameters
-capture_width = 1920
-capture_height = 1080
-capture_fps = 60
+#Supported capturing resolution and fps combinations
+res_fps_dict = {"8MP" : {"width" : 3264, "height" : 2464, "fps" : 21},
+                "6MP" : {"width" : 3264, "height" : 1848, "fps" : 28},
+                "2MP_a" : {"width" : 1920, "height" : 1080, "fps" : 30},
+                "2MP_b" : {"width" : 1640, "height" : 1232, "fps" : 30},
+                "1MP_a" : {"width" : 1280, "height" : 720, "fps" : 60},
+                "1MP_b" : {"width" : 1280, "height" : 720, "fps" : 120}}
+
+#Select a capturing mode
+mode = "1MP_b"
+capture_width = res_fps_dict[mode]["width"]
+capture_height = res_fps_dict[mode]["height"]
+capture_fps = res_fps_dict[mode]["fps"]
 
 #Displaying parameters
-display_width = 1920
-display_height = 1080
-display_fps = 60
+display_width = 1080
+display_height = 720
+display_fps = res_fps_dict[mode]["fps"]
--- scripts/cam_test.py
+++ scripts/cam_test.py
@@ -1,30 +1,35 @@
 import numpy as np
 import cv2 as cv
-import cam_params
+import cam_params as cp
 
-# Set the camera parameters
-dispW = 320
-dispH = 240
-flip = 0
-camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+#Create camera settings as formatted string
+camSet = ("nvarguscamerasrc "
+          "! video/x-raw(memory:NVMM), width={}, height={}, format=NV12, framerate={}/1 "
+          "! nvvidconv "
+          "! video/x-raw, width={}, height={}, framerate={}/1, format=BGRx "
+          "! videoconvert "
+          "! video/x-raw, format=BGR "
+          "! appsink").format(cp.capture_width,cp.capture_height,cp.capture_fps,
+                              cp.display_width,cp.display_height,cp.display_fps)
 
-# cap = cv.VideoCapture(0)
+#Create OpenCV capture object
 cap = cv.VideoCapture(camSet)
 
+#Check if camera data can be accessed
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
-    # Capture frame-by-frame
+    # Check for return value errors and read each frame
     ret, frame = cap.read()
-    # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Display the resulting frame
+    # Display frame until "q" is pressed
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
-# When everything done, release the capture
+
+#Release resources
 cap.release()
 cv.destroyAllWindows()
