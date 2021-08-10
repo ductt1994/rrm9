d748d3be8c58bb6a69daa3abcefb0f4f05934c56
Duc Thanh Tran <ductt1994@gmail.com>
2021-08-10 22:53:49 +0200

    Implemented python test script to replay rosbags

---
+++ scripts/bag_vid_replay.py
@@ -0,0 +1,23 @@
+import os
+import rosbag
+import rospy
+import cv2 as cv
+# from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError 
+
+home_dir = os.getenv("HOME")
+bag_dir = ("/bagfiles/")
+bag_name = "2021-08-10-22-32-32.bag"
+bag_full_path = home_dir + bag_dir + bag_name
+bag = rosbag.Bag(bag_full_path)
+
+
+bridge = CvBridge()
+
+for topic, img_data, t in bag.read_messages(topics=['/cam_imgs']):
+    cv_img = bridge.imgmsg_to_cv2(img_data, "bgr8")
+    cv.imshow("conv_img",cv_img)
+    # print(t)
+    cv.waitKey(60)  #after displaying the image adjust the wait time to match the capture fps
+
+bag.close()
