49c90ae80f060ebd657efb70d3ceb3f1a2da4e50
Duc Thanh Tran <ductt1994@gmail.com>
2021-08-28 16:13:26 +0200

    Testing of different rosbag

--- scripts/bag_vid_replay.py
+++ scripts/bag_vid_replay.py
@@ -7,14 +7,14 @@ from cv_bridge import CvBridge, CvBridgeError
 
 home_dir = os.getenv("HOME")
 bag_dir = ("/bagfiles/")
-bag_name = "2021-08-10-22-32-32.bag"
+bag_name = "15-08-2021 15:38:50.bag"
 bag_full_path = home_dir + bag_dir + bag_name
 bag = rosbag.Bag(bag_full_path)
 
 
 bridge = CvBridge()
 
-for topic, img_data, t in bag.read_messages(topics=['/cam_imgs']):
+for topic, img_data, t in bag.read_messages(topics=['cam_imgs']):
     cv_img = bridge.imgmsg_to_cv2(img_data, "bgr8")
     cv.imshow("conv_img",cv_img)
     # print(t)
